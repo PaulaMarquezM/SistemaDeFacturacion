194e2bf022190e3a92cbdaf2886d2f83cd9b4a37
PaulaMarquezM <paulabemar2006@gmail.com>
2024-12-20 14:06:47 -0500

    Update FrmNuevoCliente.java

--- SistemaFacturacion/src/forms/FrmNuevoCliente.java
+++ SistemaFacturacion/src/forms/FrmNuevoCliente.java
@@ -27,7 +27,7 @@ public class FrmNuevoCliente extends JDialog {
         
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setTitle(cliente == null ? "Nuevo Cliente" : "Editar Cliente");
-        setBounds(100, 100, 450, 300);
+        setBounds(100, 100, 450, 400);
         getContentPane().setLayout(new BorderLayout());
         contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
         getContentPane().add(contentPanel, BorderLayout.CENTER);
